dbee153134877d9e36a29f3c89fa811d345b7001
NuminousBlue <jdiehl2236@gmail.com>
2023-02-24 02:57:01 -0600

    thread pooling mockup for balancer

--- thread_pool/src/main.rs
+++ thread_pool/src/main.rs
@@ -1,13 +1,90 @@
+use crossbeam::channel;
+use std::thread;
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
-mod pool {
-    use std::{
-        sync::{mpsc, Arc, Mutex},
-        thread,
-    };
+enum Message {
+    NewJob(Job),
+    Terminate,
 }
 
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: channel::Sender<Message>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = channel::unbounded();
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, receiver.clone()));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        for worker in &mut self.workers {
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: channel::Receiver<Message>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            match receiver.recv().unwrap() {
+                Message::NewJob(job) => {
+                    job();
+                }
+                Message::Terminate => {
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
 
 fn main() {
-    
+    let pool = ThreadPool::new(4);
+
+    loop {
+        pool.execute(move || {
+            // Execute balance function on students Vector
+            // Upon return of any job, evaluate standard deviation
+            // If standard deviation < target, return group results,
+            // Finally, drop any remaining workers
+        });
+    }
 }
