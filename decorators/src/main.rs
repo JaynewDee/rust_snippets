f8dbe909e9295019159d6eac93e1e85038cedb47
NuminousBlue <jdiehl2236@gmail.com>
2023-03-14 15:47:31 -0500

    decorators

---
+++ decorators/src/main.rs
@@ -0,0 +1,24 @@
+use std::time::{Duration, Instant};
+
+fn timer<F, T>(mut f: F) -> impl FnMut(T) -> T {
+    move |args| {
+        let start = Instant::now();
+        f(args);
+        let elapsed = start.elapsed();
+        println!("Elapsed time: {:?}", elapsed);
+    }
+}
+
+fn cpu_spinner(x: i32) {
+    let mut nums = [x; x];
+    for i in 0..=x {
+        nums[i] = x;
+    }
+    let mut sum = 0;
+    for j in 0..nums.len() {
+        sum += nums[j];
+    }
+}
+fn main() {
+    println!("Hello, world!");
+}
