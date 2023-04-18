000f1476177528037d08b87ed0d39c9721aed891
NuminousBlue <jdiehl2236@gmail.com>
2023-04-18 01:41:40 -0500

    actix iterations

--- swervers/src/server.rs
+++ swervers/src/server.rs
@@ -1,3 +1,4 @@
+// Socket concurrency with tokio
 pub mod chat_server {
     use tokio::{
         io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
@@ -53,7 +54,9 @@ pub mod chat_server {
         }
     }
 }
-
+////////////////
+// Vanilla Concurrent HTTP
+////////////////
 pub mod http_server {
 
     use std::{
@@ -164,5 +167,114 @@ pub mod http_server {
     fn get_statics_dir(dirname: &PathBuf) -> Result<PathBuf, Box<dyn std::error::Error>> {
         let path_to_index = dirname.join("statics/index.html");
         Ok(path_to_index)
+    }
+}
+
+////////////////////
+// Actix HTTP
+pub mod swerver_framework {
+    use super::framework_socket;
+    use actix_files::NamedFile;
+    use actix_web::{middleware, web, App, Error, HttpServer, Responder};
+
+    async fn index() -> impl Responder {
+        NamedFile::open_async("./statics/index.html").await.unwrap()
+    }
+
+    #[actix_web::main]
+    async fn main() -> std::io::Result<()> {
+        env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+        log::info!("starting HTTP server at http://localhost:8787");
+
+        // Here we're creating the server and binding it to port 8080.
+        HttpServer::new(|| {
+            App::new()
+                // "/" is the path that we want to serve the `index.html` file from.
+                .service(web::resource("/").to(index))
+                .wrap(middleware::Logger::default())
+        })
+        .workers(2)
+        .bind(("127.0.0.1", 8787))?
+        .run()
+        .await
+    }
+}
+
+/////////////////////
+// Actix WebSockets
+/////////////////////
+pub mod framework_socket {
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::io::Read;
+    use std::time::{Duration, Instant};
+
+    use actix::prelude::*;
+    use actix_web::web::Bytes;
+    use actix_web_actors::ws;
+
+    const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+    const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+
+    pub struct MyWebSocket {
+        hb: Instant,
+    }
+
+    impl MyWebSocket {
+        pub fn new() -> Self {
+            Self { hb: Instant::now() }
+        }
+
+        // This function will run on an interval, every 5 seconds to check
+        // that the connection is still alive. If it's been more than
+        // 10 seconds since the last ping, we'll close the connection.
+        fn hb(&self, ctx: &mut <Self as Actor>::Context) {
+            ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+                if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
+                    ctx.stop();
+                    return;
+                }
+
+                ctx.ping(b"");
+            });
+        }
+    }
+
+    impl Actor for MyWebSocket {
+        type Context = ws::WebsocketContext<Self>;
+
+        // Start the heartbeat process for this connection
+        fn started(&mut self, ctx: &mut Self::Context) {
+            self.hb(ctx);
+        }
+    }
+
+    // The `StreamHandler` trait is used to handle the messages that are sent over the socket.
+    impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
+        // The `handle()` function is where we'll determine the response
+        // to the client's messages. So, for example, if we ping the client,
+        // it should respond with a pong. These two messages are necessary
+        // for the `hb()` function to maintain the connection status.
+        fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+            match msg {
+                // Ping/Pong will be used to make sure the connection is still alive
+                Ok(ws::Message::Ping(msg)) => {
+                    self.hb = Instant::now();
+                    ctx.pong(&msg);
+                }
+                Ok(ws::Message::Pong(_)) => {
+                    self.hb = Instant::now();
+                }
+                // Text will echo any text received back to the client (for now)
+                Ok(ws::Message::Text(text)) => ctx.text(text),
+                // Close will close the socket
+                Ok(ws::Message::Close(reason)) => {
+                    ctx.close(reason);
+                    ctx.stop();
+                }
+                _ => ctx.stop(),
+            }
+        }
     }
 }
